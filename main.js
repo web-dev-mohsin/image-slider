4c40cd2d83a5b97daf26ebb1622863aa8f514604
mohsin <mdmohsineng@gmail.com>
2022-07-02 19:48:56 +0600

    next button working fine

---
+++ main.js
@@ -0,0 +1,16 @@
+var photos = ["img/1.jpg","img/2.jpg","img/3.jpg"];
+var imgTagSelect = document.querySelector("img");
+
+
+var count = 0;
+
+function next(){
+    count++;
+    if(count >=photos.length){
+        count = 0;
+        imgTagSelect.src = photos[count];
+    }else{
+        imgTagSelect.src = photos[count];
+        
+    }
+}
